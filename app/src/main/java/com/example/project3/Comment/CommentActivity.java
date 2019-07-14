c48c975444107e46029fa72e6f5ff3a8f4da1cf8
sewon <sewon0918@kaist.ac.kr>
2019-07-14 23:19:23 +0900

    올려주겠어

--- app/src/main/java/com/example/project3/Comment/CommentActivity.java
+++ app/src/main/java/com/example/project3/Comment/CommentActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
-import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
